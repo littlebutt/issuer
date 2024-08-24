9dc859f547744a620e8fd05cc2be284b60fd20d4
littlebutt <luogan199686@gmail.com>
2024-08-24 17:03:36 +0800

    chore: 更新项目启动方式

--- scripts/start.ps1
+++ scripts/start.ps1
@@ -1,5 +1,7 @@
 # Determine the containing directory of this script
 $currentPath = Split-Path -Parent $MyInvocation.MyCommand.Definition
 $parentPath = Split-Path -Parent $currentPath
+$issuerPath = Join-Path -Path $parentPath -ChildPath "issuer"
+$mainPath = Join-Path -Path $issuerPath -ChildPath "main.py"
 
-uvicorn.exe --app-dir $parentPath "issuer.main:app"
+python.exe $mainPath
--- scripts/start.sh
+++ scripts/start.sh
@@ -1,3 +1,3 @@
 current_path=$(dirname $(pwd))
-main_path=$current_path"/issuer"
-uvicorn --app-dir $main_path "issuer.main:app"
+main_path=$current_path"/issuer/main.py"
+python3 $main_path
